526069b9ae5b1dbc89f1a965ae7b1aa36ac42fdc
Alen-Nikolov <alennikolov95@gmail.com>
2025-01-05 09:09:34 +0200

    added initial structure of the CV application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { CvComponent } from './cv/cv.component'
+import { PortfolioComponent } from './portfolio/portfolio.component'
+import { NotfoundComponent } from './notfound/notfound.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'cv' },
+    {
+        path: 'cv',
+        component: CvComponent,
+    },
+    {
+        path: 'portfolio',
+        component: PortfolioComponent,
+    },
+    { path: '404', component: NotfoundComponent },
+    { path: '**', redirectTo: '/404' },
+]
